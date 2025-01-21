c8059ea06ea910671173ded757c49d8fb89f58cb
CAMERON JOHNSON <C.D.JOHNSON-2021@hull.ac.uk>
2025-01-21 10:49:14 +0000

    Fixed errors from previous merge

--- Tankontroller/Controller/PortNumLabel.cs
+++ Tankontroller/Controller/PortNumLabel.cs
@@ -3,35 +3,30 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Tankontroller.Controller
 {
-    //--------------------------------------------------------------------------------
-    // PortNumLabel
-    //
-    // This class is used to display the port number on the screen.
-    // It contains the following:
-    //  - Texture2D[] numTextures: An array of textures for each number
-    //  - Vector2 numPosition: The position to draw the number
-    //  - int numWidth / int numHeight: The width / height of the number
-    //  - Draw: Draws the number to the screen
-    //--------------------------------------------------------------------------------
     public class PortNumLabel
     {
-        private Texture2D[] numTextures;
-        private Vector2 numPosition;
-        private int numWidth, numHeight;
+        private static readonly Texture2D[] m_PortNumbers = new Texture2D[]
+           {
+            Tankontroller.Instance().CM().Load<Texture2D>("port1"),
+            Tankontroller.Instance().CM().Load<Texture2D>("port2"),
+            Tankontroller.Instance().CM().Load<Texture2D>("port3"),
+            Tankontroller.Instance().CM().Load<Texture2D>("port4"),
+            Tankontroller.Instance().CM().Load<Texture2D>("port5"),
+            Tankontroller.Instance().CM().Load<Texture2D>("port6"),
+            Tankontroller.Instance().CM().Load<Texture2D>("port7"),
+            Tankontroller.Instance().CM().Load<Texture2D>("port8"),
+         };
+        private Rectangle m_Rectangle;
 
-        public PortNumLabel(Texture2D[] pNumTextures, Vector2 pPos, int pWidth, int pHeight)
+        public PortNumLabel(Vector2 pPos, int pWidth, int pHeight)
         {
-            numTextures = pNumTextures;
-            numPosition = pPos;
-            numWidth = pWidth;
-            numHeight = pHeight;
+            m_Rectangle = new Rectangle((int)pPos.X, (int)pPos.Y, pWidth, pHeight);
         }
 
         public void Draw(SpriteBatch pSpriteBatch, int pNumToDisplay, Color pColor)
         {
-            pSpriteBatch.Draw(numTextures[pNumToDisplay], new Rectangle((int)numPosition.X, (int)numPosition.Y, numWidth, numHeight), pColor);
+            pSpriteBatch.Draw(m_PortNumbers[pNumToDisplay], m_Rectangle, pColor);
 
         }
-
     }
 }
