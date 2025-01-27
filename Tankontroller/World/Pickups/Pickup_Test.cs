b269b313ff60b045cc4d70e12c8e4a4cce5c4044
JACK BROADLEY <J.BROADLEY-2021@hull.ac.uk>
2025-01-27 10:05:37 +0000

    Implemented the Pickup abstract class and a test child class to demonstrate how the pickup system works. Removed changes to GameScene for this commit, the pickups do work if added directly in said class however.

---
+++ Tankontroller/World/Pickups/Pickup_Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tankontroller.World.Pickups
+{
+    internal class Pickup_Test : Pickup
+    {
+        public Pickup_Test()
+            : base(Tankontroller.Instance().CM().Load<Texture2D>("circle"), new Rectangle(400, 500, 10, 10))
+        {
+        }
+
+        public void Pickup_Test_Collide(Tank tank)
+        {
+            if (Collide(tank))
+            {
+                // Stop Drawing
+                m_Active = false;
+            }
+        }
+    }
+}
