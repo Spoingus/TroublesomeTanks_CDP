d8e75b75aa370a1d58ede95184d5466b71e96ba9
Thomas Crier <ThomasCrier03@outlook.com>
2025-03-31 14:28:45 +0100

    started working on a new pickup scene

---
+++ Tankontroller/Scenes/PickupAndBulletScene.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tankontroller.Controller;
+
+namespace Tankontroller.Scenes
+{
+    public class PickupAndBulletScene : IScene
+    {
+        private Tankontroller mGameInstance;
+        private MainMenuScene mStartScene;
+        public PickupAndBulletScene(MainMenuScene startScene)
+        {
+            mStartScene = startScene;
+            mGameInstance = (Tankontroller)Tankontroller.Instance();
+        }
+
+        public override void Draw(float pSeconds)
+        {
+            
+        }
+
+        public override void Update(float pSeconds)
+        {
+            Escape();
+            mGameInstance.GetControllerManager().DetectControllers();
+
+            foreach (IController controller in mGameInstance.GetControllerManager().GetControllers())
+            {
+                controller.UpdateController();
+                if (controller.IsPressed(Control.FIRE))
+                {
+                    IGame game = Tankontroller.Instance();
+                    game.GetControllerManager().SetAllTheLEDsWhite();
+                    game.SM().Transition(null);
+                }
+            }
+        }
+        public override void Escape()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                mGameInstance.SM().Transition(mStartScene, true);
+            }
+        }
+
+
+    }
+}
